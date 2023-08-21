70bf0efa454b68aa1c687146c7e8e39c96bc23e5
david <david@divlogic.net>
2023-08-21 15:58:21 -0600

    add playwright test for movement

---
+++ playwright-tests/example.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect, Page } from '@playwright/test'
+
+async function getSprites(page: Page) {
+  const spritesHandle = await page.evaluateHandle(() => {
+    return window.sprites
+  })
+
+  return await spritesHandle.jsonValue()
+}
+test('Can move using wasd', async ({ page }) => {
+  await page.goto('localhost:5173')
+
+  const starting = (await getSprites(page)).data[0]
+
+  const startingX = starting._wx
+  const startingY = starting._wy
+
+  await page.keyboard.press('a', { delay: 1000 })
+  const afterMovingLeft = (await getSprites(page)).data[0]
+  const afterLeftX = afterMovingLeft._wx
+  const afterLeftY = afterMovingLeft._wy
+  await expect(afterLeftX).toBeLessThan(startingX)
+  await expect(afterLeftY).toBe(startingY)
+
+  await page.keyboard.press('s', { delay: 1000 })
+  const afterMovingDown = (await getSprites(page)).data[0]
+  const afterDownX = afterMovingDown._wx
+  const afterDownY = afterMovingDown._wy
+  await expect(afterDownX).toBe(afterLeftX)
+  await expect(afterDownY).toBeGreaterThan(afterLeftY)
+
+  await page.keyboard.press('d', { delay: 1000 })
+  const afterMovingRight = (await getSprites(page)).data[0]
+  const afterRightX = afterMovingRight._wx
+  const afterRightY = afterMovingRight._wy
+  await expect(afterRightX).toBeGreaterThan(afterDownX)
+  await expect(afterRightY).toBe(afterDownY)
+
+  await page.keyboard.press('w', { delay: 1000 })
+  const afterMovingUp = (await getSprites(page)).data[0]
+  const afterUpX = afterMovingUp._wx
+  const afterUpY = afterMovingUp._wy
+  await expect(afterUpX).toBe(afterRightX)
+  await expect(afterUpY).toBeLessThan(afterRightY)
+})
