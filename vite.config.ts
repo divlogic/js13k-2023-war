f0267b5e8a914ac9b6f0e0378f48a49c9b6b9b9b
david <david@divlogic.net>
2023-08-24 18:49:49 -0600

    add js13k-vite-plugins to vite config
    shrunk the output by over 1kB

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
+import { js13kViteConfig } from 'js13k-vite-plugins'
+import { defineConfig } from 'vite'
 import kontra from 'rollup-plugin-kontra'
-export default {
-  build: {
-    rollupOptions: {
-      treeshake: true,
-      // entry: 'main.js',
-      // dest: 'bundle.js',
-      output: {
-        compact: true,
-        format: 'iife',
+
+export default defineConfig(
+  js13kViteConfig({
+    // closureOptions: { preserveOutput: true }, // WIP still not working
+    closureOptions: false,
+    viteOptions: {
+      rollupOptions: {
+        plugins: [
+          kontra({
+            gameObject: {
+              // enable only velocity and rotation functionality
+              velocity: true,
+              rotation: true,
+              acceleration: true,
+              ttl: true,
+              group: true,
+            },
+            vector: {
+              // enable vector length functionality
+              length: true,
+              clamp: true,
+            },
+            // turn on debugging
+            debug: false,
+          }),
+        ],
       },
-      plugins: [
-        kontra({
-          gameObject: {
-            // enable only velocity and rotation functionality
-            velocity: true,
-            rotation: true,
-            acceleration: true,
-            ttl: true,
-            group: true,
-          },
-          vector: {
-            // enable vector length functionality
-            length: true,
-            clamp: true,
-          },
-          // turn on debugging
-          debug: false,
-        }),
-      ],
     },
-  },
-}
+  })
+)
