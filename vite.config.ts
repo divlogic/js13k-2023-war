6b35c7b942eb8cb8870341883f19603cf0f75675
david <david@divlogic.net>
2023-08-24 22:11:35 -0600

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,23 @@
-import { js13kViteConfig } from 'js13k-vite-plugins'
+import {
+  advzipPlugin,
+  ectPlugin,
+  googleClosurePlugin,
+  defaultViteBuildOptions,
+  roadrollerPlugin,
+} from 'js13k-vite-plugins'
 import { defineConfig } from 'vite'
-import kontra from 'rollup-plugin-kontra'
 
-export default defineConfig(
-  js13kViteConfig({
-    // closureOptions: { preserveOutput: true }, // WIP still not working
-    closureOptions: false,
-    viteOptions: {
-      rollupOptions: {
-        plugins: [
-          kontra({
-            gameObject: {
-              // enable only velocity and rotation functionality
-              velocity: true,
-              rotation: true,
-              acceleration: true,
-              ttl: true,
-              group: true,
-            },
-            vector: {
-              // enable vector length functionality
-              length: true,
-              clamp: true,
-            },
-            // turn on debugging
-            debug: false,
-          }),
-        ],
-      },
-    },
-  })
-)
+/**
+ * Closure is useful but easily the most finicky of the bunch.
+ * Breaks if I import all of kontra, but switching to a named import
+ * has fixed it so far as well as bring it down by around 6 kB.
+ */
+export default defineConfig({
+  build: defaultViteBuildOptions,
+  plugins: [
+    googleClosurePlugin(),
+    roadrollerPlugin(),
+    ectPlugin(),
+    advzipPlugin(),
+  ],
+})
