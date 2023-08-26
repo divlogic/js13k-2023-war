6246d4ec5013fbb009d5b6cbc36fc86686f845e2
david <david@divlogic.net>
2023-08-26 16:23:10 -0600

    format

--- jest-tests/outputSize.test.ts
+++ jest-tests/outputSize.test.ts
@@ -1,12 +1,13 @@
-import { lstat } from 'node:fs/promises'
-import { describe, expect, test } from '@jest/globals'
+import { lstat } from 'node:fs/promises';
+import path from 'node:path';
+import { describe, expect, test } from '@jest/globals';
 
 describe('Package', () => {
   test('The final bundle size should not hit 13kb', async () => {
-    const maxBites = 13312
-    const fileInfo = await lstat(__dirname + '/../dist/index.zip')
-    const size = fileInfo.size
+    const maxBites = 13312;
+    const fileInfo = await lstat(path.join(__dirname, '/../dist/index.zip'));
+    const size = fileInfo.size;
 
-    expect(size).toBeLessThan(maxBites)
-  }, 20000)
-})
+    expect(size).toBeLessThan(maxBites);
+  }, 20000);
+});
