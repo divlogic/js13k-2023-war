30326db75e3d82b099474ad5f50ec1ef430f7b81
david <david@divlogic.net>
2023-08-29 14:38:23 -0600

    format npc-behavior.ts

--- playwright-tests/scenarios/npc-behavior.ts
+++ playwright-tests/scenarios/npc-behavior.ts
@@ -5,45 +5,47 @@ import kontra, {
   initPointer,
   onKey,
   Pool,
-} from 'kontra'
-import SpriteState from '../../src/SpriteState'
-import { Character } from '../../src/character'
-import { detectCollisions, handleBounds } from '../../src/collisionDetection'
-import { Weapon } from '../../src/weapon'
-import { NPC } from '../../src/npc'
+} from 'kontra';
+import SpriteState from '../../src/SpriteState';
+import { Character } from '../../src/character';
+import { detectCollisions, handleBounds } from '../../src/collisionDetection';
+import { Weapon } from '../../src/weapon';
+import { NPC } from '../../src/npc';
 declare global {
   interface Window {
-    sprites: any
-    player: Character
-    enemy: NPC
+    sprites: any;
+    player: Character;
+    enemy: NPC;
   }
 }
 
 export function npcBehaviorScene() {
-  const { canvas } = init()
+  const { canvas } = init();
 
-  kontra.initKeys()
-  initPointer()
+  kontra.initKeys();
+  initPointer();
 
-  const sprites = new SpriteState()
+  const sprites = new SpriteState();
 
   const player = new Character({
     x: 300,
     y: 300,
     player: true,
     team: 'blue',
-  })
-  window.player = player
+  });
+  window.player = player;
 
-  sprites.push(player)
+  sprites.push(player);
 
-  const enemy = new NPC({ x: 0, y: 0, moveSpeed: 1 })
-  window.enemy = enemy
-  sprites.push(enemy)
+  const enemy = new NPC({ x: 0, y: 0, moveSpeed: 1 });
+  window.enemy = enemy;
+  sprites.push(enemy);
+
+  enemy.addTarget(player);
 
   // This is taken from the example, might be a bug in their type file
   // @ts-ignore
-  let pool = Pool({ create: Sprite })
+  let pool = Pool({ create: Sprite });
 
   const fireLance = new Weapon({
     pool,
@@ -54,43 +56,43 @@ export function npcBehaviorScene() {
     y: 5,
     render(this: Sprite) {
       if (this.context != null) {
-        this.context.fillStyle = 'brown'
-        this.context.beginPath()
-        this.context.fillRect(0, 0, 15, 2)
-        this.context.fillStyle = 'green'
-        this.context.fillRect(15, 0, 8, 4)
-        this.context.stroke()
+        this.context.fillStyle = 'brown';
+        this.context.beginPath();
+        this.context.fillRect(0, 0, 15, 2);
+        this.context.fillStyle = 'green';
+        this.context.fillRect(15, 0, 8, 4);
+        this.context.stroke();
       }
     },
-  })
-  player.addChild(fireLance)
+  });
+  player.addChild(fireLance);
 
   const loop = GameLoop({
     // fps: 1,
     update: function (this: GameLoop) {
-      sprites.refresh()
+      sprites.refresh();
       sprites.forEach((sprite) => {
-        handleBounds(sprite, canvas)
-      })
+        handleBounds(sprite, canvas);
+      });
 
       // collision detection
-      detectCollisions(sprites)
+      detectCollisions(sprites);
 
-      sprites.filter((sprite) => sprite.isAlive())
+      sprites.filter((sprite) => sprite.isAlive());
     },
     render: function () {
       sprites.forEach((sprite) => {
-        sprite.render()
-      })
+        sprite.render();
+      });
     },
-  })
+  });
 
   onKey('esc', () => {
     if (loop.isStopped) {
-      loop.start()
+      loop.start();
     } else {
-      loop.stop()
+      loop.stop();
     }
-  })
-  loop.start()
+  });
+  loop.start();
 }
