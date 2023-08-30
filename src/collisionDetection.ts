d7c837ee429427219c9b885ce7ce424ae3d8f62e
david <david@divlogic.net>
2023-08-29 18:35:14 -0600

    add proximity detection to collisionDetection

--- src/collisionDetection.ts
+++ src/collisionDetection.ts
@@ -1,22 +1,40 @@
-import { Sprite } from 'kontra'
-import SpriteState from './SpriteState'
+import { Sprite } from 'kontra';
+import SpriteState from './SpriteState';
 // import { createAsteroid } from './main'
 
 export function detectCollisions(sprites: SpriteState) {
   sprites.forEach((sprite) => {
-    const nearBySprites = sprites.quadTree.get(sprite) as Sprite[]
+    const nearBySprites = sprites.quadTree.get(sprite) as Sprite[];
     nearBySprites.forEach((neighbor) => {
-      if (neighbor.type === 'character' && neighbor.team !== sprite.team) {
-        let dx = neighbor.x - sprite.x
-        let dy = neighbor.y - sprite.y
-        const hyp = Math.hypot(dx, dy)
-        if (hyp < sprite.radius + sprite.radius) {
-          neighbor.ttl = 0
-          sprite.ttl = 0
+      if (
+        sprite.type === 'character' &&
+        neighbor.type === 'character' &&
+        neighbor.team !== sprite.team
+      ) {
+        const dx = neighbor.x - sprite.x;
+        const dy = neighbor.y - sprite.y;
+        const hyp = Math.hypot(dx, dy);
+        if (hyp < 100) {
+          if (neighbor.addTarget) {
+            if (neighbor.target === undefined || neighbor.target === null)
+              neighbor.addTarget(sprite);
+          }
+        }
+      } else if (
+        neighbor.type === 'character' &&
+        neighbor.team !== sprite.team
+      ) {
+        const dx = neighbor.x - sprite.x;
+        const dy = neighbor.y - sprite.y;
+        const hyp = Math.hypot(dx, dy);
+        if (hyp < sprite.radius + neighbor.radius) {
+          console.log(sprite.radius, neighbor.radius);
+          neighbor.ttl = 0;
+          sprite.ttl = 0;
         }
       }
-    })
-  })
+    });
+  });
 }
 
 export function handleBounds(sprite: Sprite, canvas: HTMLCanvasElement) {
@@ -26,20 +44,20 @@ export function handleBounds(sprite: Sprite, canvas: HTMLCanvasElement) {
     0,
     canvas.width - sprite.width,
     canvas.height - sprite.height
-  )
-  const radius: number = sprite.radius
+  );
+  const radius: number = sprite.radius;
   if (sprite.x < -radius) {
-    sprite.x = canvas.width + radius
+    sprite.x = canvas.width + radius;
   } else if (sprite.x > canvas.width + radius) {
     // sprite is beyond the right edge
     // sprite.x = 0 - radius
   }
   // sprite is beyond the top edge
   if (sprite.y < -radius) {
-    sprite.y = canvas.height + radius
+    sprite.y = canvas.height + radius;
   } else if (sprite.y > canvas.height + radius) {
     // sprite is beyond the bottom edge
-    sprite.y = -radius
+    sprite.y = -radius;
   }
-  sprite.update()
+  sprite.update();
 }
