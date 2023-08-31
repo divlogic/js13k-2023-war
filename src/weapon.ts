1b55e6ecdc343bf82cbdd256534a30c184c6c80e
David <david@divlogic.net>
2023-08-30 18:16:34 -0600

    format weapon.ts

--- src/weapon.ts
+++ src/weapon.ts
@@ -1,22 +1,23 @@
-import { Pool, Sprite, SpriteClass, pointerPressed } from 'kontra'
-import SpriteState from './SpriteState'
+import { type Pool, type Sprite, SpriteClass, pointerPressed } from 'kontra';
+import type SpriteState from './SpriteState';
 
 export class Weapon extends SpriteClass {
-  projectileWeapon?: boolean = false
-  pool: Pool
-  sprites: SpriteState
+  projectileWeapon?: boolean = false;
+  pool: Pool;
+  sprites: SpriteState;
 
   constructor(properties: any) {
-    console.log('Constructing weapon')
-    super(properties)
-    this.pool = properties.pool
-    this.sprites = properties.sprites
-    this.projectileWeapon = properties.projectileWeapon
+    console.log('Constructing weapon');
+    super(properties);
+    this.pool = properties.pool;
+    this.sprites = properties.sprites;
+    this.projectileWeapon = properties.projectileWeapon;
   }
+
   attack(this: Weapon) {
     if (this.projectileWeapon) {
-      const cos = Math.cos(this.world.rotation)
-      const sin = Math.sin(this.world.rotation)
+      const cos = Math.cos(this.world.rotation);
+      const sin = Math.sin(this.world.rotation);
       const bullet = this.pool.get({
         team: this.parent?.team,
         type: 'bullet',
@@ -29,18 +30,19 @@ export class Weapon extends SpriteClass {
         radius: 2,
         width: 2,
         height: 2,
-      })
+      });
 
       // I think calling this line will be highly dependent on
       // what kind of weapon it is, like sword vs projectile
       //   this.addChild(bullet)
-      this.sprites.push(bullet as Sprite)
+      this.sprites.push(bullet as Sprite);
     }
   }
+
   update(dt?: number | undefined): void {
-    super.update(dt)
+    super.update(dt);
     if (pointerPressed('left')) {
-      this.attack()
+      this.attack();
     }
   }
 }
