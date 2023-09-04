a83239c423d8118c5afd0da80b2bac965cd7b680
David <david@divlogic.net>
2023-09-04 15:42:00 -0600

    wip build out armor

--- src/sprites/armorPlate.ts
+++ src/sprites/armorPlate.ts
@@ -3,42 +3,72 @@ import { Sprite } from 'kontra';
 export function createArmorPlate(radius: number) {
   return Sprite({
     render() {
-      this.drawBezier();
+      this.drawBezier('right');
+      this.drawBezier('left');
     },
-    drawBezier() {
+    drawBezier(side: string) {
+      let yModifier;
+      let xModifier;
+      if (side === 'right') {
+        yModifier = 1;
+      } else if (side === 'left') {
+        yModifier = -1;
+      }
+
       if (this.context != null) {
         const context = this.context;
+        context.moveTo(0, 0);
+        context.beginPath();
+        context.strokeStyle = 'grey';
+        context.arc(0, 0, 2, 0, 2 * Math.PI);
+        context.stroke();
         context.strokeStyle = 'black';
         context.beginPath();
+
         const x = -1 * radius;
-        const y = -2 * radius;
-        context.moveTo(x, y);
-        context.beginPath();
-        context.moveTo(x, y);
+        const y = 2 * yModifier * radius;
         const x2 = radius;
-        const y2 = -2 * radius;
+        const y2 = 2 * yModifier * radius;
+
+        context.moveTo(x, y);
+
+        context.beginPath();
+        context.arc(x, y, 1, 0, 2 * Math.PI);
+        context.strokeStyle = 'orange';
+        context.stroke();
+        context.beginPath();
+
+        context.strokeStyle = 'black';
+
+        context.moveTo(x, y);
         context.bezierCurveTo(
           x,
-          y - (1 / 2) * radius,
+          y + yModifier * radius,
           x2,
-          y2 - (1 / 2) * radius,
+          y2 + yModifier * radius,
           x2,
           y2
         );
 
-        const offset = 5;
-        context.lineTo(x2, y2 + offset);
+        const yOffset = 0.8 * radius * yModifier;
+        context.lineTo(x2, y2 + yOffset);
         context.bezierCurveTo(
           x2,
-          y2 + offset - (1 / 2) * radius,
+          y2 + yOffset + 0.5 * yModifier * radius,
           x,
-          y + offset - (1 / 2) * radius,
+          y + yOffset + 0.5 * yModifier * radius,
           x,
-          y + offset
+          y + yOffset
         );
         context.closePath();
 
         context.stroke();
+
+        context.beginPath();
+        context.arc(x2, y2, 1, 0, 2 * Math.PI);
+        context.strokeStyle = 'green';
+        context.stroke();
+        context.beginPath();
       }
     },
   });
