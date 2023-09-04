c5a1a74f45fc4de70c60bebbdd734ab06076e6d7
David <david@divlogic.net>
2023-09-04 15:04:54 -0600

    wip build out armor plate sprite

--- src/sprites/armorPlate.ts
+++ src/sprites/armorPlate.ts
@@ -10,11 +10,10 @@ export function createArmorPlate(radius: number) {
         const context = this.context;
         context.strokeStyle = 'black';
         context.beginPath();
-        context.moveTo(-1 * radius, -2 * radius);
-        context.stroke();
-        context.beginPath();
         const x = -1 * radius;
         const y = -2 * radius;
+        context.moveTo(x, y);
+        context.beginPath();
         context.moveTo(x, y);
         const x2 = radius;
         const y2 = -2 * radius;
@@ -26,6 +25,19 @@ export function createArmorPlate(radius: number) {
           x2,
           y2
         );
+
+        const offset = 5;
+        context.lineTo(x2, y2 + offset);
+        context.bezierCurveTo(
+          x2,
+          y2 + offset - (1 / 2) * radius,
+          x,
+          y + offset - (1 / 2) * radius,
+          x,
+          y + offset
+        );
+        context.closePath();
+
         context.stroke();
       }
     },
