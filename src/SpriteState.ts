ab704bea6c4c3313165f14d87cfdaed2835bf6fd
David <david@divlogic.net>
2023-09-07 14:59:08 -0600

    fix SpriteState lint errors

--- src/SpriteState.ts
+++ src/SpriteState.ts
@@ -1,42 +1,42 @@
-import { Sprite, Quadtree } from 'kontra'
+import { type Sprite, Quadtree } from 'kontra';
 
 // I may wish to use a Pool with pool.getAliveObjects()
 // instead of the array method I'm doing now.
 export default class SpriteState {
-  data: Sprite[]
-  quadTree: Quadtree
+  data: Sprite[];
+  quadTree: Quadtree;
   constructor(initialSprites: Sprite[] = []) {
-    this.data = initialSprites
-    this.quadTree = Quadtree()
+    this.data = initialSprites;
+    this.quadTree = Quadtree();
   }
 
-  filter(callback: (sprite: Sprite) => any) {
-    this.data = this.data.filter(callback)
+  filter(callback: (sprite: Sprite) => boolean): void {
+    this.data = this.data.filter(callback);
   }
 
-  forEach(callback: (sprite: Sprite) => any) {
-    this.data.forEach(callback)
+  forEach(callback: (sprite: Sprite) => void): void {
+    this.data.forEach(callback);
   }
 
   push(sprite: Sprite): void {
-    this.data.push(sprite)
-    this.quadTree.add(sprite)
+    this.data.push(sprite);
+    this.quadTree.add(sprite);
     if (import.meta.env.DEV) {
-      window.sprites = this
+      window.sprites = this;
     }
   }
 
-  get(index: number) {
-    const sprite = this.data[index]
-    return sprite
+  get(index: number): Sprite {
+    const sprite = this.data[index];
+    return sprite;
   }
 
   length(): number {
-    return this.data.length
+    return this.data.length;
   }
 
   refresh(): void {
-    this.quadTree.clear()
-    this.quadTree.add(this.data)
+    this.quadTree.clear();
+    this.quadTree.add(this.data);
   }
 }
