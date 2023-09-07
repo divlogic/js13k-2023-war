8623e5c5ec525bd86a1ade1e7a76b54cb0e5e530
David <david@divlogic.net>
2023-09-06 22:06:04 -0600

    change how helmet renders, use single sprite instead of many

--- src/sprites/helmet.ts
+++ src/sprites/helmet.ts
@@ -1,15 +1,33 @@
-import { Sprite, SpriteClass, degToRad, movePoint, randInt } from 'kontra';
+import { SpriteClass, degToRad, movePoint, randInt } from 'kontra';
+import type { SpriteProps } from '../character';
+
+interface accessories {
+  startX: number;
+  startY: number;
+  endX: number;
+  endY: number;
+  cp1x: number;
+  cp1y: number;
+  cp2x: number;
+  cp2y: number;
+}
+
+type HelmetProperties = SpriteProps & {
+  radius: number;
+  accessories?: accessories[];
+};
 
 export class Helmet extends SpriteClass {
   radius: number;
+  accessories?: accessories[];
 
-  constructor(properties: any = {}) {
+  constructor(properties: HelmetProperties) {
     super(properties);
     this.radius = properties.radius ?? 2;
+    this.team = properties.team ?? 'red';
     // This whole accessories thing is intended to be a plume
     const accessories = [];
-    console.log('constructor');
-    for (let i = 0; i < randInt(20, 25); i++) {
+    for (let i = 0; i < randInt(23, 25); i++) {
       const startX = 0;
       const startY = 0;
       const endX = randInt(-30, -15);
@@ -18,48 +36,25 @@ export class Helmet extends SpriteClass {
       const cp1y = 0;
       const cp2x = endX;
       const cp2y = 0;
-      accessories.push(
-        Sprite({
-          render() {
-            this.draw();
-            if (this.context != null) {
-              const context = this.context;
-              context.moveTo(startX, startY);
-              context.beginPath();
-              context.strokeStyle = 'blue';
-              context.arc(cp1x, cp1y, 1, 0, 2 * Math.PI);
-              context.stroke();
-              context.fill();
-
-              context.moveTo(endX, endY);
-              context.beginPath();
-              context.strokeStyle = 'green';
-              context.arc(cp2x, cp2y, 1, 0, 2 * Math.PI);
-              context.stroke();
-              context.fill();
-
-              context.strokeStyle = 'red';
-              context.moveTo(startX, startY);
-              context.beginPath();
-              context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, endX, endY);
-              context.stroke();
-            }
-          },
-        })
-      );
+      accessories.push({
+        startX,
+        startY,
+        endX,
+        endY,
+        cp1x,
+        cp1y,
+        cp2x,
+        cp2y,
+      });
     }
-    console.log('accessories.length is: ', accessories.length);
-    this.addChild(accessories);
+    this.accessories = accessories;
   }
 
   draw(): void {
     super.draw();
 
     this.drawHelmet();
-    // this.drawAccessories();
-    // this.drawAccessories();
-    // this.drawAccessories();
-    // this.drawAccessories();
+    this.drawAccessories();
   }
 
   drawHelmet(): void {
@@ -67,9 +62,9 @@ export class Helmet extends SpriteClass {
       const context = this.context;
       const start = degToRad(40);
       const end = degToRad(320);
-      const radius = 12;
+      const radius = this.radius;
 
-      context.strokeStyle = 'black';
+      context.strokeStyle = 'grey';
       context.beginPath();
       const startPoint = movePoint({ x: 0, y: 0 }, start, 5);
       const endPoint = movePoint({ x: 0, y: 0 }, end, 5);
@@ -79,6 +74,37 @@ export class Helmet extends SpriteClass {
       context.lineTo(endPoint.x, endPoint.y);
       context.lineTo(startPoint.x, startPoint.y);
       context.stroke();
+      context.fillStyle = 'grey';
+      context.fill();
+    }
+  }
+
+  drawAccessories(): void {
+    if (this.context != null) {
+      const context = this.context;
+      const team = this.team;
+      this.accessories?.forEach((accessory) => {
+        // debug circle
+        // context.moveTo(startX, startY);
+        // context.beginPath();
+        // context.strokeStyle = 'orange';
+        // context.arc(cp1x, cp1y, 1, 0, 2 * Math.PI);
+        // context.stroke();
+        // context.fill();
+
+        context.strokeStyle = team;
+        context.moveTo(accessory.startX, accessory.startY);
+        context.beginPath();
+        context.bezierCurveTo(
+          accessory.cp1x,
+          accessory.cp1y,
+          accessory.cp2x,
+          accessory.cp2y,
+          accessory.endX,
+          accessory.endY
+        );
+        context.stroke();
+      });
     }
   }
 }
