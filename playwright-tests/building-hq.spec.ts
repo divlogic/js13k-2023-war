f9cfb94434f0ee92397f39ad9ee351e4fb8ee504
David <david@divlogic.net>
2023-09-07 18:48:35 -0600

    fix test errors

--- playwright-tests/building-hq.spec.ts
+++ playwright-tests/building-hq.spec.ts
@@ -1,15 +1,25 @@
 import { expect, test } from '@playwright/test';
 import { getObject } from './utils';
+import type SpriteState from '../src/SpriteState';
+import type { Building } from '../src/sprites/building';
 
 test.describe('Given that an HQ building exists', async () => {
   test('If it has a team, it should spawn units', async ({ page }) => {
-    await page.goto('localhost:5173?scenario=buildingHq');
-    const startSprites = await getObject(page, 'sprites');
-    expect(startSprites.length).toBe(0);
+    await page.goto('localhost:5173?scenario=buildingHq', {
+      waitUntil: 'load',
+    });
 
-    const hq = await getObject(page, 'hq');
+    const startSprites = (await getObject(page, 'sprites')) as SpriteState;
+    expect(startSprites.data.length).toBe(1);
 
-    expect(hq).toBeDefined();
+    await expect
+      .poll(async () => {
+        return await getObject(page, 'hq');
+      })
+      .toBeDefined();
+
+    const hq = (await getObject(page, 'hq')) as Building;
+
     // Arbitrary team color
     expect(hq.team).toBe('green');
 
@@ -18,17 +28,18 @@ test.describe('Given that an HQ building exists', async () => {
     await expect
       .poll(
         async () => {
-          const sprites = await getObject(page, 'sprites');
-          return sprites.length();
+          const sprites = (await getObject(page, 'sprites')) as SpriteState;
+          return sprites.data.length;
         },
         {
           message: 'There should be more sprites after some time',
           timeout: 5000,
         }
       )
-      .toBeGreaterThan(0);
-    const endSprites = await getObject(page, 'sprites');
-    endSprites.forEach((element) => {
+      .toBeGreaterThan(1);
+
+    const endSprites = (await getObject(page, 'sprites')) as SpriteState;
+    endSprites.data.forEach((element) => {
       // Sprites should be the same team as their HQ's
       expect(element.team).toBe('green');
     });
