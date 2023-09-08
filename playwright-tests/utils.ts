aa5e68a41ef8e55c31940157c54eb1f729eb8136
David <david@divlogic.net>
2023-09-07 18:23:52 -0600

    refactor utils.ts
    add return type
    remove unused code

--- playwright-tests/utils.ts
+++ playwright-tests/utils.ts
@@ -1,6 +1,6 @@
-import { Page } from '@playwright/test';
+import type { Page } from '@playwright/test';
 
-export async function getSprites(page: Page) {
+export async function getSprites(page: Page): Promise<unknown> {
   const spritesHandle = await page.evaluateHandle(() => {
     return window.sprites;
   });
@@ -8,26 +8,8 @@ export async function getSprites(page: Page) {
   return await spritesHandle.jsonValue();
 }
 
-export async function getObject(page: Page, key: string): any {
+export async function getObject(page: Page, key: string): Promise<unknown> {
   const obj = await page.evaluate('window.' + key);
 
   return obj;
-}
-
-export class ValueChangeTracker {
-  values: number[] = [];
-  correctDirection: boolean = false;
-
-  getterFunction(callback) {
-    this.values.push(callback());
-  }
-  runCalculations(operator: string) {
-    return this.values.reduceRight((previous, current, currentIndex, arr) => {
-      if (currentIndex === arr.length - 1) {
-      }
-      if (previous <= current) {
-        return current;
-      }
-    });
-  }
 }
