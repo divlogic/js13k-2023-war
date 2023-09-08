dc039a365d683233b97f2b096761edcf4609c4fd
David <david@divlogic.net>
2023-09-08 17:27:59 -0600

    wip add test for npc seek and destroy behavior

---
+++ playwright-tests/npc-seek-and-destroy.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+import type { NPC } from '../src/npc';
+
+test.describe('NPC will default to seek and destroy mode', async () => {
+  test('Seek and destroy will make them move towards enemy buildings', async ({
+    page,
+  }) => {
+    await page.goto('localhost:5173?scenario=npcSeekAndDestroy');
+    await page.waitForTimeout(1000);
+
+    const blueSprite = await page.evaluate(() => {
+      return window.sprites.data.find((sprite) => {
+        return sprite.color === 'blue';
+      });
+    });
+
+    expect(blueSprite?.target).toBeDefined();
+    expect(blueSprite?.target?.type).toBe('spawner');
+  });
+});
