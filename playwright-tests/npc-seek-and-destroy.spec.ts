e13c0163961d7adc505e8c47a50fc8f740f4bef9
David <david@divlogic.net>
2023-09-10 00:14:54 -0600

    update npc-seek-and-destroy.spec.ts

--- playwright-tests/npc-seek-and-destroy.spec.ts
+++ playwright-tests/npc-seek-and-destroy.spec.ts
@@ -5,15 +5,19 @@ test.describe('NPC will default to seek and destroy mode', async () => {
     page,
   }) => {
     await page.goto('localhost:5173?scenario=npcSeekAndDestroy');
+
+    const blueSprite = await page.evaluate('window.blueSprite');
+    expect(blueSprite.position._x).toBe(1);
+    expect(blueSprite.position._y).toBe(500);
+
     await page.waitForTimeout(1000);
-
-    const blueSprite = await page.evaluate(() => {
-      return window.sprites.data.find((sprite) => {
-        return sprite.color === 'blue';
-      });
-    });
-
-    expect(blueSprite?.target).toBeDefined();
-    expect(blueSprite?.target?.type).toBe('spawner');
+    const updatedBlueSprite = await page.evaluate('window.blueSprite');
+    expect(updatedBlueSprite?.target).toBeDefined();
+    expect(updatedBlueSprite?.target?.type).toBe('spawner');
+    expect(updatedBlueSprite?.mode).toBe('seekAndDestroy');
+    // They are positioned diagonally apart to start
+    // This will indicate the sprite has moved closer
+    expect(updatedBlueSprite?.position?._x).toBeGreaterThan(300);
+    expect(updatedBlueSprite?.position?._y).toBeLessThan(300);
   });
 });
